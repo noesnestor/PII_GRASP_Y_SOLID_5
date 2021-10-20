cd0e3e656aefd141ad35332d5d4482b813f8ba9a
Néstor Martínez <nestor.marvi@gmail.com>
2021-10-20 19:18:57 -0300

    Aplicado Patron DIP

--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -4,9 +4,9 @@ namespace Full_GRASP_And_SOLID
 {
     public class FilePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+        public void PrintRecipe(ISaleContent saleContent)
         {
-            File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
+            File.WriteAllText("Recipe.txt", saleContent.GetTextToPrint());
         }
     }
 }
